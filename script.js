626209e09d701dd81f25275b0e9653e10c70fe38
Toni <gebonutrition@gmail.com>
2026-09-02 11:18:27 +0300

    fix source links

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ const AMAZON_URLS = {
   meta:
     "https://www.amazon.com/dp/B0GTWB11LW?maas=maas_adg_B05D582BAB7E5BC8F40DEBA1EBC61AEE_afap_abs&ref_=aa_maas&tag=maas",
 
-  facebook:
-    "https://www.amazon.com/dp/B0GTWB11LW?maas=maas_adg_B05D582BAB7E5BC8F40DEBA1EBC61AEE_afap_abs&ref_=aa_maas&tag=maas",
-
-  klavio:
-    "https://www.amazon.com/dp/B0GTWB11LW?maas=maas_adg_25B95FF8BD805EF1A9DF6465C32333F9_afap_abs&ref_=aa_maas&tag=maas",
-
   klaviyo:
     "https://www.amazon.com/dp/B0GTWB11LW?maas=maas_adg_25B95FF8BD805EF1A9DF6465C32333F9_afap_abs&ref_=aa_maas&tag=maas",
 
